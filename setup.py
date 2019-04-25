0cb004f1d408913bb5fe301a1a1e00064b0f00f1
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2019-04-25 15:35:05 +0200

    added requirements and readme to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
+from os.path import dirname, abspath, join
+
+base_path = dirname(abspath(__file__))
+
+with open(join(base_path, "README.md")) as readme_file:
+    readme = readme_file.read()
+
+with open(join(base_path, "requirements.txt")) as req_file:
+    requirements = req_file.readlines()
 
 setup(
     name='koerby',
     version='0.1',
     description='rdf based data integration framework',
+    long_description=readme,
     author='diggr',
     author_email='p.muehleder@ub.uni-leipzig.de',
+    install_requires=requirements,
     license='MIT',
     packages=['koerby'],
     entry_points="""
